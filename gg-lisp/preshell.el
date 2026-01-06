1719118b6add49b02b337f71a7801f742e7e71a3
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2026-01-06 18:49:16 +0100

    new package preshell, runs predefined shell commands

---
+++ gg-lisp/preshell.el
@@ -0,0 +1,22 @@
+;;; preshell
+
+
+(defcustom preshell-commands-alist
+  '(("ping localhost" . "ping -c3 127.0.0.1"))
+  "Alist of predefined shell commands for `preshell'.")
+
+
+(defun preshell ()
+  "Prompt for a predefined shell command in `preshell-commands-alist' and run it asynchronously."
+  (interactive)
+  (let* ((choice (completing-read
+                  "Preshell: "
+                  preshell-commands-alist
+                  nil
+                  t))
+         (command (cdr (assoc choice preshell-commands-alist))))
+    (when command
+      (async-shell-command command))))
+
+
+(provide 'preshell)
