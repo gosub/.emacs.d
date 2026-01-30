30cc42a85e0435908cc2270f0991169bf67947ca
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2026-01-30 20:46:37 +0100

    moved erlang stuff to an aux elisp file

---
+++ gg-lisp/gg-erlang-aux.el
@@ -0,0 +1,16 @@
+;;; gg-erlang-aux
+
+(defun gg/require-erlang-from-distro ()
+  (if (file-directory-p "/usr/lib/erlang")
+      (let* ((lib-dir "/usr/lib/erlang/lib/")
+             (tools-dir (file-name-completion "tools" lib-dir))
+             (erlang-mode-dir (concat lib-dir tools-dir "emacs")))
+	(add-to-list 'load-path erlang-mode-dir)
+	(add-to-list 'exec-path "/usr/lib/erlang/bin")
+	(setq erlang-root-dir "/usr/lib/erlang")
+	(require 'erlang-start)))
+  ;; printable char set is unicode
+  (setq inferior-erlang-machine-options
+	'("+pc" "unicode")))
+
+(provide 'gg-erlang-aux)
