064e43479365cb0a50af791af9f3dc098bb55194
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2026-02-01 11:12:02 +0100

    Update gg-org-yt to use process-lines instead of shell-command-to-string

--- gg-lisp/gg-org-yt.el
+++ gg-lisp/gg-org-yt.el
@@ -3,17 +3,21 @@
 (defun gg/yt-playlist-to-org (playlist-url)
   "Turn a YouTube playlist link into an org section, where each video is a subsection."
   (interactive "sPlaylist url or id: ")
-  (let*
-      ((header-cmd (concat "yt-dlp"
-			   " --ignore-errors --get-filename "
-			   " --output '* [/] [[https://www.youtube.com/playlist?list=%(playlist_id)s][%(uploader)s - %(playlist_title)s]]' "
-			   " --playlist-end 1 "))
-       (playlist-header (shell-command-to-string (concat header-cmd playlist-url)))
-       (entries-cmd (concat "yt-dlp"
-			    " --ignore-errors --get-filename "
-			    " --output '- [ ] [[https://www.youtube.com/watch?v=%(id)s][%(title)s]]' "))
-       (playlist-entries (shell-command-to-string (concat entries-cmd playlist-url))))
-    (insert (concat "\n" playlist-header playlist-entries "\n"))))
+  (let* ((header-lines
+          (process-lines
+           "yt-dlp" "--ignore-errors" "--get-filename"
+           "--output"
+           "* [/] [[https://www.youtube.com/playlist?list=%(playlist_id)s][%(uploader)s - %(playlist_title)s]]"
+           "--playlist-end" "1" playlist-url))
+         (playlist-header (mapconcat #'identity header-lines "\n"))
+         (entries-lines
+          (process-lines
+           "yt-dlp" "--ignore-errors" "--get-filename"
+           "--output"
+           "- [ ] [[https://www.youtube.com/watch?v=%(id)s][%(title)s]]"
+           playlist-url))
+         (playlist-entries (mapconcat #'identity entries-lines "\n")))
+    (insert (concat "\n" playlist-header "\n" playlist-entries "\n"))))
 
 (provide 'gg-org-yt)
 ;;; gg-org-yt.el ends here
