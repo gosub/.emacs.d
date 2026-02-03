7913e0fa66892d368ce254d58020ea7fcc1bdf83
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2026-02-03 08:38:12 +0100

    Refactor gg-websearch with DWIM function and URL encoding

--- gg-lisp/gg-websearch.el
+++ gg-lisp/gg-websearch.el
@@ -1,22 +1,37 @@
-(defun gg/search-on-youtube ()
-  "search region text on youtube"
-  (interactive)
+;;; gg-websearch.el --- Search the web from Emacs -*- lexical-binding: t; -*-
+
+;;; Code:
+
+(require 'url-util)
+
+(defun gg/search-on-youtube (query)
+  "Search QUERY on YouTube."
   (browse-url (concat "https://www.youtube.com/results?search_query="
-		      (buffer-substring (mark) (point)))))
+                      (url-hexify-string query))))
 
-
-(defun gg/search-on-google ()
-  "search region text on google"
-  (interactive)
+(defun gg/search-on-google (query)
+  "Search QUERY on Google."
   (browse-url (concat "https://www.google.com/search?q="
-		      (buffer-substring (mark) (point)))))
+                      (url-hexify-string query))))
 
-
-(defun gg/search-on-hackernews ()
-  "search region text on hacker news"
-  (interactive)
+(defun gg/search-on-hackernews (query)
+  "Search QUERY on Hacker News."
   (browse-url (concat "https://hn.algolia.com/?q="
-		      (buffer-substring (mark) (point)))))
+                      (url-hexify-string query))))
 
+(defun gg/websearch-dwim ()
+  "Search the web with selected engine.
+If region is active, search for it; otherwise prompt for query."
+  (interactive)
+  (let* ((engines '(("google" . gg/search-on-google)
+                    ("youtube" . gg/search-on-youtube)
+                    ("hn" . gg/search-on-hackernews)))
+         (engine (completing-read "Engine: " engines nil t))
+         (query (if (use-region-p)
+                    (buffer-substring-no-properties (region-beginning) (region-end))
+                  (read-string "Search: ")))
+         (fn (cdr (assoc engine engines))))
+    (funcall fn query)))
 
 (provide 'gg-websearch)
+;;; gg-websearch.el ends here
