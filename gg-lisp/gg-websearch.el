3439a7dd54b80ad00c2e4747225229b91b9214c1
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2026-02-16 20:53:37 +0100

    Rename gg/ prefix to gg- in gg-websearch.el

--- gg-lisp/gg-websearch.el
+++ gg-lisp/gg-websearch.el
@@ -4,28 +4,28 @@
 
 (require 'url-util)
 
-(defun gg/search-on-youtube (query)
+(defun gg-search-on-youtube (query)
   "Search QUERY on YouTube."
   (browse-url (concat "https://www.youtube.com/results?search_query="
                       (url-hexify-string query))))
 
-(defun gg/search-on-google (query)
+(defun gg-search-on-google (query)
   "Search QUERY on Google."
   (browse-url (concat "https://www.google.com/search?q="
                       (url-hexify-string query))))
 
-(defun gg/search-on-hackernews (query)
+(defun gg-search-on-hackernews (query)
   "Search QUERY on Hacker News."
   (browse-url (concat "https://hn.algolia.com/?q="
                       (url-hexify-string query))))
 
-(defun gg/websearch-dwim ()
+(defun gg-websearch-dwim ()
   "Search the web with selected engine.
 If region is active, search for it; otherwise prompt for query."
   (interactive)
-  (let* ((engines '(("google" . gg/search-on-google)
-                    ("youtube" . gg/search-on-youtube)
-                    ("hn" . gg/search-on-hackernews)))
+  (let* ((engines '(("google" . gg-search-on-google)
+                    ("youtube" . gg-search-on-youtube)
+                    ("hn" . gg-search-on-hackernews)))
          (engine (completing-read "Engine: " engines nil t))
          (query (if (use-region-p)
                     (buffer-substring-no-properties (region-beginning) (region-end))
