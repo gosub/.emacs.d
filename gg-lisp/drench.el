fe67b641fb94e438a710e74ff6c0e0fa551b0150
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2026-03-01 15:26:30 +0100

    drench: replace color legend with keybinding reference

--- gg-lisp/drench.el
+++ gg-lisp/drench.el
@@ -134,11 +134,9 @@
               (insert (propertize " " 'face face 'drench-cell (cons row col)))))))
       (insert "\n"))
     (insert (format "\nmoves left: %d\n" (drench-remaining-moves)))
-    (dotimes (i 6)
-      (let* ((val  (1+ i))
-             (face (aref drench-face-syms val)))
-        (insert (propertize (format " %d " val) 'face face))))
-    (insert "\n")))
+    (insert "\n")
+    (insert "1-6  select color\n")
+    (insert "q    quit\n")))
 
 
 (defun drench-get-square (row column)
