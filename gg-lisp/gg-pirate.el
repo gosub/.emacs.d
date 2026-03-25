2b268d6b4729f75d8da052a3e117f99921409779
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2026-03-25 18:55:58 +0100

    gg-pirate: add destination folder selection (~/dl/ or /mnt/usb/)

--- gg-lisp/gg-pirate.el
+++ gg-lisp/gg-pirate.el
@@ -1,18 +1,23 @@
 ;;; gg-pirate.el --- Media download utilities -*- lexical-binding: t; -*-
 
-(defun gg-movie-pirate (url name)
-  "Asynchronously download URL to ~/dl/NAME.mkv using ffmpeg.
+(defvar gg-movie-pirate-destinations
+  '("~/dl/" "/mnt/usb/")
+  "List of destination directories for `gg-movie-pirate'.")
+
+(defun gg-movie-pirate (url name dest)
+  "Asynchronously download URL to DEST/NAME.mkv using ffmpeg.
 NAME is transformed to lowercase and spaces are replaced with underscores."
   (interactive
    (list
     (read-string "URL: ")
-    (read-string "Name: ")))
+    (read-string "Name: ")
+    (completing-read "Destination: " gg-movie-pirate-destinations nil t)))
   (let* ((safe-name (replace-regexp-in-string
                      " +" "_"
                      (downcase name)))
          (output (expand-file-name
                   (concat safe-name ".mkv")
-                  (expand-file-name "dl" (getenv "HOME"))))
+                  (expand-file-name dest)))
          (process-name (format "ffmpeg-%s" safe-name)))
     (start-process
      process-name
