5b0f2c1ea244f8b7c9d395c991fa066e74134f89
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2026-04-03 13:01:02 +0200

    inc-at-point-gg: support numeric prefix argument for count

--- gg-lisp/inc-at-point-gg.el
+++ gg-lisp/inc-at-point-gg.el
@@ -1,24 +1,24 @@
 ;;; inc-at-point-gg.el --- Increment/decrement number at point -*- lexical-binding: t; -*-
 
-(defun inc-at-point-gg ()
-  "Increment number at point, partially simulating C-a in vim."
-  (interactive)
+(defun inc-at-point-gg (count)
+  "Increment number at point by COUNT, partially simulating C-a in vim."
+  (interactive "p")
   (save-excursion
     (skip-chars-backward "0123456789")
     (unless (looking-at "[0123456789]+")
       (error "No number at point"))
     (replace-match (number-to-string
-                    (1+ (string-to-number (match-string 0)))))))
+                    (+ (string-to-number (match-string 0)) count)))))
 
-(defun dec-at-point-gg ()
-  "Decrement number at point, partially simulating C-x in vim."
-  (interactive)
+(defun dec-at-point-gg (count)
+  "Decrement number at point by COUNT, partially simulating C-x in vim."
+  (interactive "p")
   (save-excursion
     (skip-chars-backward "0123456789")
     (unless (looking-at "[0123456789]+")
       (error "No number at point"))
     (replace-match (number-to-string
-                    (1- (string-to-number (match-string 0)))))))
+                    (- (string-to-number (match-string 0)) count)))))
 
 (provide 'inc-at-point-gg)
 ;;; inc-at-point-gg.el ends here
